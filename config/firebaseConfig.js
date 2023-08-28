20fa3ba2eacd53bf89281c87f698926a46dd46f3
dbsdud00 <dbsdudsw@icloud.com>
2023-08-28 13:31:09 +0900

    end

---
+++ config/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCaFqeuOQ6poDvTPVQfgk76Ll3hzainke8",
+  authDomain: "react-dbsdud00.firebaseapp.com",
+  projectId: "react-dbsdud00",
+  storageBucket: "react-dbsdud00.appspot.com",
+  messagingSenderId: "348222335910",
+  appId: "1:348222335910:web:a2954838bd327f5c523d67",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
